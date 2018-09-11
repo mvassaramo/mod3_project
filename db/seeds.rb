1971571152812d9dc9650f1aad74ac8a1aea5f2c
github username <github email address>
2018-09-11 14:38:56 +0100

    updated seed date

--- db/seeds.rb
+++ db/seeds.rb
@@ -8,11 +8,12 @@
 Template.destroy_all
 
 p "Starting seed"
-orange_template = Template.create(name: "Orange", level: "Easy", image_url: "https://res.cloudinary.com/mvassaramo/image/upload/v1536593483/Image_templates/orange.svg")
 butterfly_template = Template.create(name: "Butterfly", level: "Easy", image_url: "https://res.cloudinary.com/mvassaramo/image/upload/v1536599392/Image_templates/animal-1769728.svg")
 tree_template = Template.create(name: "Tree", level: "Easy", image_url: "https://res.cloudinary.com/mvassaramo/image/upload/v1536663128/Image_templates/Tree.svg")
 abstract_pattern_template = Template.create(name: "Abstract Pattern", level: "Easy", image_url: "https://res.cloudinary.com/mvassaramo/image/upload/v1536663425/Image_templates/Abstract-pattern.svg")
 rose_template = Template.create(name: "Rose", level: "Easy", image_url: "https://res.cloudinary.com/mvassaramo/image/upload/v1536663643/Image_templates/Rose.svg")
+floral_pattern_template = Template.create(name: "Floral Pattern", level: "Medium", image_url: "https://res.cloudinary.com/mvassaramo/image/upload/v1536664076/Image_templates/Floral-pattern.svg")
+decorative_pattern_template = Template.create(name: "Decorative Pattern", level: "Medium", image_url: "https://res.cloudinary.com/mvassaramo/image/upload/v1536664300/Image_templates/decorative-1769539.svg")
 
 
 
