fc2268f5b86ddb0235613635537288bcb0599d92
github username <github email address>
2018-09-13 12:21:39 +0100

    updated gallery images

--- db/seeds.rb
+++ db/seeds.rb
@@ -12,7 +12,11 @@ butterfly_template = Template.create(name: "Butterfly", level: "Medium", image_u
 tree_template = Template.create(name: "Tree", level: "Easy", image_url: "https://res.cloudinary.com/mvassaramo/image/upload/v1536741735/Image_templates/tree-latest.svg")
 abstract_pattern_template = Template.create(name: "Abstract Pattern", level: "Complex", image_url: "https://res.cloudinary.com/mvassaramo/image/upload/v1536740679/Image_templates/abstract-latest.svg")
 rose_template = Template.create(name: "Rose", level: "Easy", image_url: "https://res.cloudinary.com/mvassaramo/image/upload/v1536692541/Image_templates/rose-latest.svg")
+fox_template = Template.create(name: "Fox", level: "Easy", image_url: "https://res.cloudinary.com/mvassaramo/image/upload/v1536748675/Image_templates/fox.svg")
+lion_template = Template.create(name: "Lion", level: "Easy", image_url: "https://res.cloudinary.com/mvassaramo/image/upload/v1536765215/Image_templates/lion-temp.svg")
+lotus_template = Template.create(name: "Lotus", level: "Easy", image_url: "https://res.cloudinary.com/mvassaramo/image/upload/v1536766233/Image_templates/lotus.svg")
+elephant_template = Template.create(name: "Elephant", level: "Complex", image_url: "https://res.cloudinary.com/mvassaramo/image/upload/v1536769950/Image_templates/elephant.svg")
+swirl_template = Template.create(name: "Swirl", level: "Easy", image_url: "https://res.cloudinary.com/mvassaramo/image/upload/v1536767919/Image_templates/swirl.svg")
+decor_template = Template.create(name: "Decor", level: "Medium", image_url: "https://res.cloudinary.com/mvassaramo/image/upload/v1536771069/Image_templates/decor.svg")
 
-
-# p cub_template
 p "Finished seeding"
